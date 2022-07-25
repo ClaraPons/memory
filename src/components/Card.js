cbac635ef1bcd9192c6472f023308f32b8fe9a1f
Omnivers <Twersimor.stock@outlook.com>
2022-07-25 15:38:45 +0200

    Organisation des fichiers && Bootstrap

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Card extends React.Component {
+	render() {
+		return(
+         <div className='card m-4 col-6 '>
+
+        </div>
+		)
+	}
+}
+
+export default Card;
