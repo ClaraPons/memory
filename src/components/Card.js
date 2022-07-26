91bcdc769390b8361ea6e6307b9d131bd6243dae
Emad0007 <BASEMRF007@GMAIL.COM>
2022-07-26 11:22:20 +0100

    commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ class Card extends React.Component {
 	constructor(){
 		super()
 		this.state={
-		  cards:[1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10]
+		  cards:[1,2,3,4,5,6,7,8,9,10]
 		}
 	  }
 	handleCards = () => {
