490f866b5400cfac55d3a7dfdcb675b204d0ddc6
Omnivers <Twersimor.stock@outlook.com>
2022-07-26 16:16:14 +0200

    gameRules

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import image0 from '../images/js.png'
 import image1 from '../images/NODE.png'
 import image2 from '../images/benoit-pic.png'
 import image3 from '../images/julie-pic.png'
-import image4 from '../images/milk.jpeg'
+import image4 from '../images/baby.jpg'
 import image5 from '../images/oldman.png'
 import image6 from '../images/bootstrap.png'
 import image7 from '../images/gigachad-meme.png'
@@ -15,15 +15,26 @@ class Card extends React.Component {
   constructor() {
     super()
     this.state = {
-      cards: [{front:image0,returned:false}, {front:image1,returned:false},{front:image2,returned:false}, {front:image3,returned:false},{front:image4,returned:false}, {front:image5,returned:false},{front:image6,returned:false}, {front:image7,returned:false},{front:image8,returned:false}, {front:image9,returned:false}],
-    }
+      cards: [{front:image0,returned:false}, {front:image1,returned:false},{front:image2,returned:false}, {front:image3,returned:false},{front:image4,returned:false}, {front:image5,returned:false},{front:image6,returned:false}, {front:image7,returned:false},{front:image8,returned:false}, {front:image9,returned:false},{front:image0,returned:false}, {front:image1,returned:false},{front:image2,returned:false}, {front:image3,returned:false},{front:image4,returned:false}, {front:image5,returned:false},{front:image6,returned:false}, {front:image7,returned:false},{front:image8,returned:false}, {front:image9,returned:false}],
+	  returned:null,
+	}
   }
   handleCards = (card, index) => {
 	const taskClone = [...this.state.cards]
     taskClone[index].returned = true
-    this.setState({cards: taskClone})
+    this.setState({cards: taskClone, returned:this.state.returned+1})
+	this.gameRules()
   }
-
+  gameRules = () =>{
+	if(this.state.returned>1){
+		this.state.cards.forEach(card => {
+			card.returned=false
+		});
+		this.setState({
+			returned:null,
+		})
+	}
+  }
   shuffleCards =(array)=>{
 	let currentIndex = array.length,  randomIndex
 	while (currentIndex != 0) {
