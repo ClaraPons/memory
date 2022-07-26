fc29ca5683e835c747872099a50a373a8708b955
Omnivers <Twersimor.stock@outlook.com>
2022-07-26 02:36:20 +0200

    Images without maping

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,5 @@
 import React from 'react'
 import image from "../card-game.png"
-const cards=[{
-	src:image,
-	src:image,
-	src:image,
-	src:image,
-	src:image,
-	src:image,
-	src:image,
-	src:image,
-	src:image,
-	src:image,
-	src:image,
-	src:image,
-	src:image,
-	src:image,
-	src:image,
-	src:image,
-	src:image,
-	src:image,
-	src:image,
-	src:image}]
 
 class Card extends React.Component {
 	render() {
@@ -29,9 +8,26 @@ class Card extends React.Component {
 		{this.props.isSubmitted &&
          <div className='content pt-3'>
 				<div className='row col-10'>
-					{cards.map((card)=>(
-					 <img src={card.src} alt="Back front" style={{width: "8rem"}}/>
-						))}
+					<img src={image} alt='Back' style={{width: "8rem"}}/>
+					<img src={image} alt='Back' style={{width: "8rem"}}/>
+					<img src={image} alt='Back' style={{width: "8rem"}}/>
+					<img src={image} alt='Back' style={{width: "8rem"}}/>
+					<img src={image} alt='Back' style={{width: "8rem"}}/>
+					<img src={image} alt='Back' style={{width: "8rem"}}/>
+					<img src={image} alt='Back' style={{width: "8rem"}}/>
+					<img src={image} alt='Back' style={{width: "8rem"}}/>
+					<img src={image} alt='Back' style={{width: "8rem"}}/>
+					<img src={image} alt='Back' style={{width: "8rem"}}/>
+					<img src={image} alt='Back' style={{width: "8rem"}}/>
+					<img src={image} alt='Back' style={{width: "8rem"}}/>
+					<img src={image} alt='Back' style={{width: "8rem"}}/>
+					<img src={image} alt='Back' style={{width: "8rem"}}/>
+					<img src={image} alt='Back' style={{width: "8rem"}}/>
+					<img src={image} alt='Back' style={{width: "8rem"}}/>
+					<img src={image} alt='Back' style={{width: "8rem"}}/>
+					<img src={image} alt='Back' style={{width: "8rem"}}/>
+					<img src={image} alt='Back' style={{width: "8rem"}}/>
+					<img src={image} alt='Back' style={{width: "8rem"}}/>
 				</div>
         </div>
 		}
