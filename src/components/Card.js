8dc88c8d538c8c8f3f010b478b0a4bc53623f0c4
Clara Pons <clarapons@air-de-clara.home>
2022-07-26 11:07:09 +0200

    Map cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,33 +2,28 @@ import React from 'react'
 import image from "../card-game.png"
 
 class Card extends React.Component {
+	constructor(){
+		super()
+		this.state={
+		  cards:[1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10]
+		}
+	  }
+
+	handleCards = () => {
+
+	}
+
 	render() {
 		return(
 		<>
 		{this.props.isSubmitted &&
          <div className='content pt-3'>
-				<div className='row col-10'>
-					<img src={image} alt='Back' style={{width: "8rem"}}/>
-					<img src={image} alt='Back' style={{width: "8rem"}}/>
-					<img src={image} alt='Back' style={{width: "8rem"}}/>
-					<img src={image} alt='Back' style={{width: "8rem"}}/>
-					<img src={image} alt='Back' style={{width: "8rem"}}/>
-					<img src={image} alt='Back' style={{width: "8rem"}}/>
-					<img src={image} alt='Back' style={{width: "8rem"}}/>
-					<img src={image} alt='Back' style={{width: "8rem"}}/>
-					<img src={image} alt='Back' style={{width: "8rem"}}/>
-					<img src={image} alt='Back' style={{width: "8rem"}}/>
-					<img src={image} alt='Back' style={{width: "8rem"}}/>
-					<img src={image} alt='Back' style={{width: "8rem"}}/>
-					<img src={image} alt='Back' style={{width: "8rem"}}/>
-					<img src={image} alt='Back' style={{width: "8rem"}}/>
-					<img src={image} alt='Back' style={{width: "8rem"}}/>
-					<img src={image} alt='Back' style={{width: "8rem"}}/>
-					<img src={image} alt='Back' style={{width: "8rem"}}/>
-					<img src={image} alt='Back' style={{width: "8rem"}}/>
-					<img src={image} alt='Back' style={{width: "8rem"}}/>
-					<img src={image} alt='Back' style={{width: "8rem"}}/>
+				<div className='row col-9'>
+				{this.state.cards.map((card) =>
+					<img src={image} alt='Back' style={{width: "8rem"}} onClick={this.handleCards}/>
+				)}
 				</div>
+				
         </div>
 		}
 		</>
