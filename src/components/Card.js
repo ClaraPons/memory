7196909618aed36819d4ed00b435531250962519
Omnivers <Twersimor.stock@outlook.com>
2022-07-27 12:38:28 +0200

    End of game

--- src/components/Card.js
+++ src/components/Card.js
@@ -17,7 +17,7 @@ class Card extends React.Component {
     super()
     this.state = {
       cards: [
-		{front:image0,returned:false, id:0}, {front:image1,returned:false, id:1},{front:image2,returned:false, id:2}, {front:image3,returned:false, id:3},{front:image4,returned:false, id:4}, {front:image5,returned:false, id:5},{front:image6,returned:false, id:6}, {front:image7,returned:false, id:7},{front:image8,returned:false, id:8}, {front:image9,returned:false, id:9},{front:image0,returned:false, id:0}, {front:image1,returned:false, id:1},{front:image2,returned:false, id:2}, {front:image3,returned:false, id:3},{front:image4,returned:false, id:4}, {front:image5,returned:false, id:5},{front:image6,returned:false, id:6}, {front:image7,returned:false, id:7},{front:image8,returned:false, id:8}, {front:image9,returned:false, id:9}
+		{front:image0,returned:false, id:0}, {front:image0,returned:false, id:0},{front:image3,returned:false, id:3}, {front:image3,returned:false, id:3},
 	],
 	  returned:null,
 	  comparaison:[],
@@ -25,18 +25,25 @@ class Card extends React.Component {
 	} 
   }
   handleCards = (index) => {
-	this.setState({score:this.state.score+1})
 	const cardsClone = [...this.state.cards]
 	const clonedComparaison=[...this.state.comparaison,cardsClone[index]]
     cardsClone[index].returned = true
-    this.setState({cards: cardsClone, returned:this.state.returned+1, comparaison:clonedComparaison})
+    this.setState({cards: cardsClone, returned:this.state.returned+1, comparaison:clonedComparaison, score:this.state.score+1})
+  }
+  componentDidUpdate(handleCards){
 	this.gameRules()
   }
   deleteCards=()=>{	 
 	  if(this.state.comparaison.length===2 && this.state.comparaison[0].id===this.state.comparaison[1].id){
-		this.setState({cards: this.state.cards.filter(card => { 
-			return	 card !== this.state.comparaison[0] && card!== this.state.comparaison[1]})});
+		setTimeout(()=>{
+			this.setState({cards: this.state.cards.filter(card => { 
+			return	 card !== this.state.comparaison[0] && card!== this.state.comparaison[1]})});},500)
 		}
+	   else if(this.state.cards.length===2){
+			// this.setState({
+			// 	cards:[]
+			// })
+	   }
 	   else{
 		this.setState({
 			comparaison:[]
@@ -44,10 +51,12 @@ class Card extends React.Component {
 	   }
   }
   gameRules = () =>{
-	if(this.state.returned>1){
-		this.state.cards.forEach(card => {
+	if(this.state.returned===2){
+		setTimeout(()=>{
+			this.state.cards.forEach(card => {
 			card.returned=false
 		});
+	    },300)
 		this.setState({
 			returned:null,
 		})
